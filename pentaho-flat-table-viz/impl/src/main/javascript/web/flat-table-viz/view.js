ffd1981caad3af8ca0552a175fdef3a6bd05fda1
marcoslarsen <marcoslarsen@gmail.com>
2016-07-11 12:22:13 +0100

    [BACKLOG-9161] - Refactor AMD module to have one with and another without css dependencies

--- pentaho-flat-table-viz/impl/src/main/javascript/web/flat-table-viz/view.js
+++ pentaho-flat-table-viz/impl/src/main/javascript/web/flat-table-viz/view.js
@@ -14,124 +14,15 @@
  * limitations under the License.
  */
 define([
-  "pentaho/visual/base/View",
-  "underscore",
-  "jquery",
-  "datatables.net",
+  "./view_def",
   "datatables.net-bs",
-  "datatables.net-fixedheader",
-  "datatables.net-colreorder",
-  "datatables.net-scroller",
   "css!bootstrap/dist/css/bootstrap.css",
   "css!datatables.net-bs/css/dataTables.bootstrap.css",
   "css!datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css",
   "css!datatables.net-colreorder-bs/css/colReorder.bootstrap.css",
   "css!datatables.net-scroller-bs/css/scroller.bootstrap.css"
-], function(BaseView, _, $) {
+], function(View) {
   "use strict";
 
-  return BaseView.extend({
-
-    /** @override */
-    _init: function() {
-      this.base();
-
-      this.tElement = $('<table class="table table-striped table-bordered nowrap" cellspacing="0" width="100%">');
-
-      $(this._element).empty();
-      $(this._element).append(this.tElement);
-    },
-
-    /** @override */
-    _render: function() {
-
-      var cols = this.model.getv("columns");
-      var fCols = filterCols(cols._values.attributes._elems);
-
-      var data = this.model.getv("data");
-      var tData = parseTableData(data, fCols);
-
-      var iScroll = this.model.getv("scroller");
-      var fHeader = this.model.getv("fixedHeader");
-
-      if(this.dTable)this.dTable.destroy();
-      this.tElement.empty();
-
-      if(!tData.columns.length) return;
-      this.dTable = this.tElement.DataTable({
-
-        data:             tData.data,
-        columns:          tData.columns,
-
-        // Built-in features not exposed and disabled by default
-        paging:           iScroll ? true : false,
-        info:             false,
-        filter:           false,
-
-        deferRender:      iScroll ? true : false,
-        scrollCollapse:   iScroll ? true : false,
-
-        // Features exposed on viz model
-        fixedHeader:      iScroll ? false : fHeader,
-        ordering:         this.model.getv("ordering"),
-        colReorder:       this.model.getv("colReorder"),
-        scroller:         this.model.getv("scroller"),
-        scrollY:          this.model.getv("scrollY")
-      });
-
-      this.dTable.colReorder.order( tData.order );
-      this._resize();
-    },
-
-    /** @override */
-    _resize: function() {
-
-      var w  = this.model.getv("width");
-      var h = this.model.getv("height");
-
-      $(this._element).css({ width: w, height: h });
-      this.dTable.columns.adjust().draw(false);
-    },
-
-    /** @override */
-    dispose: function() {
-      this.base();
-    }
-  });
-
-  function filterCols(cols) {
-
-    var filteredCols = [];
-
-    _.each(cols, function(col, c){
-      filteredCols.push(col._values.name._value);
-    });
-
-    return filteredCols;
-  }
-
-  function parseTableData(data, filteredCols) {
-
-    var tData = { data: [], columns: [], order: [] };
-
-    _.each(filteredCols, function(col){
-      _.each(data.model.attributes, function(attr, a){
-        if(attr.name == col) tData.order.push(a);
-      });
-    });
-
-    _.each(data.model.attributes, function(attr, a){
-      var cTitle = attr.name;
-      var cVisible = !filteredCols.length || filteredCols.indexOf(cTitle) !== -1 ? true : false;
-      tData.columns.push({title: cTitle, visible: cVisible});
-      if(tData.order.indexOf(a) == -1) tData.order.push(a);
-    });
-
-    _.each(data.implem.rows, function(row, r){
-      var rData = _.map(row.c, function(cData, c){ return cData.v; });
-      tData.data.push(rData);
-    });
-
-    return tData;
-  }
+  return View;
   });
